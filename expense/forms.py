e5736f9e8c5a40938e21400a01e2c6a350f0edab
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-05-07 21:09:57 +0600

    forms.py

--- expense/forms.py
+++ expense/forms.py
@@ -1,24 +1,23 @@
 from django import forms
-from .models import Transaction
+from .models import Transaction,Category
 
 
 class TransactionDebitForm(forms.ModelForm):
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}),label=' Category')
+    dr = forms.FloatField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter Debit amount(00.00)'}),label=' Amount')
     class Meta:
         model = Transaction
         fields = ['title', 'cat', 'dr']
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'cat': forms.NumberInput(attrs={'class': 'form-control'}),
-            'date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-            'dr': forms.Textarea(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control','placeholder':'Expense Title'})
         }
 class TransactionCreditForm(forms.ModelForm):
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}),label='Category')
+    cr = forms.FloatField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter Debit amount(00.00)'}),label='Amount')
+    
     class Meta:
         model = Transaction
         fields = ['title', 'cat', 'cr']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'cat': forms.NumberInput(attrs={'class': 'form-control'}),
-            'date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-            'dr': forms.Textarea(attrs={'class': 'form-control'}),
         }
