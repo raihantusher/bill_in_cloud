2a6e9eeb9b5f7d49e8db9c35658934c1d3b9e115
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-05-07 21:17:31 +0600

    models.py

--- expense/models.py
+++ expense/models.py
@@ -27,11 +27,11 @@ class Transaction(models.Model):
 
 
 #model for expenses list
-class ExpensesList(models.Model):
-    expense_category = models.CharField(max_length=100)
-    amount_of_money = models.DecimalField(max_digits=10, decimal_places=2)
-    entry_date = models.DateField()
-    description = models.TextField()
+# class ExpensesList(models.Model):
+#     expense_category = models.CharField(max_length=100)
+#     amount_of_money = models.DecimalField(max_digits=10, decimal_places=2)
+#     entry_date = models.DateField()
+#     description = models.TextField()
 
-    def __str__(self):
-        return self.expense_category
+#     def __str__(self):
+#         return self.expense_category
