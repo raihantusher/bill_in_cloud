3b18d1bb809d2ddc3caf856944e9304c37335e93
Raihan Tusher <raihan.tusher@yahoo.com>
2023-11-25 20:32:19 +0600

    url names

--- bookkeeper/urls.py
+++ bookkeeper/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import AllReportsView, IncomeStatement
 
 urlpatterns = [
-    path("report/", AllReportsView.as_view()),
-    path("report/income_statement/", IncomeStatement.as_view()),
+    path("report/", AllReportsView.as_view(), name="report"),
+    path("report/income_statement/", IncomeStatement.as_view(), name="income_statement"),
 ]
